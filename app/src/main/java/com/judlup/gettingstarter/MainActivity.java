9579f03e381ec880e99fcc5239af28b4c9f8b245
Judlup Luns <judlup@gmail.com>
2020-11-01 11:16:59 -0500

    01-11-2020 | validar campos en registro | Judlup

--- app/src/main/java/com/judlup/gettingstarter/MainActivity.java
+++ app/src/main/java/com/judlup/gettingstarter/MainActivity.java
@@ -6,29 +6,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv1;
-    private ListView lv1;
+    private EditText etn, etp;
 
-    private  String nombres [] = {"Samuel","Valentina","Martín"};
-    private  String edades [] = {"18","25","30"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etn = (EditText) findViewById(R.id.etn);
+        etp = (EditText) findViewById(R.id.etp);
     }
 
-    public void clickImagen1(View view){
-        Toast.makeText(this,"Imagen 1",Toast.LENGTH_SHORT).show();
-    }
+    public void Registrar(View view){
+        String nombre = etn.getText().toString();
+        String password = etp.getText().toString();
 
-    public void clickImagen2(View view){
-        Toast.makeText(this,"Imagen 2",Toast.LENGTH_SHORT).show();
+        if(nombre.length() == 0){
+            Toast.makeText(this, "Debes ingresar un nombre", Toast.LENGTH_SHORT).show();
+        }
+
+        if(password.length() == 0){
+            Toast.makeText(this, "Debes ingresar un password", Toast.LENGTH_SHORT).show();
+        }
+
+        if(nombre.length() != 0 && password.length() != 0){
+            Toast.makeText(this, "Registro en proceso", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
