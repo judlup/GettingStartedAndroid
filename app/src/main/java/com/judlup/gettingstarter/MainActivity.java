3119fbc11a3bb8240040e26f8aff85cdec3a109b
Judlup Luns <judlup@gmail.com>
2020-11-01 10:07:25 -0500

    01-11-2020 | ejemplo list view con click | Judlup

--- app/src/main/java/com/judlup/gettingstarter/MainActivity.java
+++ app/src/main/java/com/judlup/gettingstarter/MainActivity.java
@@ -4,62 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et1, et2;
-    private Spinner sp1;
     private TextView tv1;
+    private ListView lv1;
 
+    private  String nombres [] = {"Samuel","Valentina","Martín"};
+    private  String edades [] = {"18","25","30"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = (EditText)findViewById(R.id.valor_1);
-        et2 = (EditText)findViewById(R.id.valor_2);
         tv1 = (TextView)findViewById(R.id.tv1);
-        sp1 = (Spinner) findViewById(R.id.sp1);
+        lv1 = (ListView) findViewById(R.id.lv1);
 
-        String [] opciones = {"Sumar","Restar","Multiplicar","Dividir"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_view_content, nombres);
+        lv1.setAdapter(adapter);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.spinner_text_opcion, opciones);
-        sp1.setAdapter(adapter);
-    }
-
-    public void Calcular(View view){
-        String Valor_1 = et1.getText().toString();
-        String Valor_2 = et2.getText().toString();
-
-        int Valor_1_int = Integer.parseInt(Valor_1);
-        int Valor_2_int = Integer.parseInt(Valor_2);
-
-        String seleccion = sp1.getSelectedItem().toString();
-        String resultado = "";
-        if(seleccion.equals("Sumar") == true){
-            int suma = Valor_1_int + Valor_2_int;
-            resultado = "La suma es: "+suma;
-        }else if(seleccion.equals("Restar") == true){
-            int resta = Valor_1_int - Valor_2_int;
-            resultado = resultado+" La resta es: "+resta;
-        }else if(seleccion.equals("Multiplicar") == true){
-            int multiplicar = Valor_1_int * Valor_2_int;
-            resultado = resultado+" La multiplicación es: "+multiplicar;
-        }else if(seleccion.equals("Dividir") == true){
-            if(Valor_2_int == 0){
-                Toast.makeText(this,"El segundo valor no puede ser cero",Toast.LENGTH_SHORT).show();
-            }else{
-                int dividir = Valor_1_int / Valor_2_int;
-                resultado = resultado+" La división es: "+dividir;
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                tv1.setText("La edad de "+lv1.getItemAtPosition(position)+" es "+edades[position]+" años");
             }
-        }
-
-        tv1.setText(resultado);
+        });
     }
 }
