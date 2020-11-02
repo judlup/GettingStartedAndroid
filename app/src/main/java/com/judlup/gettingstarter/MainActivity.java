e048707bb86b1b44163a9fdb342c18ade5efc633
Judlup Luns <judlup@gmail.com>
2020-11-01 21:00:16 -0500

    01-11-2020 | Registrar y Buscar artículo | Judlup

--- app/src/main/java/com/judlup/gettingstarter/MainActivity.java
+++ app/src/main/java/com/judlup/gettingstarter/MainActivity.java
@@ -2,9 +2,12 @@ package com.judlup.gettingstarter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,36 +19,65 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et1, et2;
+    private EditText et_codigo, et_descripcion, et_precio ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = (EditText)findViewById(R.id.et1);
-        et2 = (EditText)findViewById(R.id.et2);
+        et_codigo = (EditText)findViewById(R.id.et_codigo);
+        et_descripcion = (EditText)findViewById(R.id.et_descripcion);
+        et_precio = (EditText)findViewById(R.id.et_precio);
     }
 
-    public void guardar(View view){
-        String nombre = et1.getText().toString();
-        String datos = et2.getText().toString();
-        SharedPreferences preferences = getSharedPreferences("agenda", Context.MODE_PRIVATE);
-        SharedPreferences.Editor obj_editor = preferences.edit();
-        obj_editor.putString(nombre,datos);
-        obj_editor.commit();
-        Toast.makeText(this, "El contacto ha sido guardado",Toast.LENGTH_SHORT).show();
+    public void registrar(View view){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+        SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
+
+        String codigo = et_codigo.getText().toString();
+        String descripcion = et_descripcion.getText().toString();
+        String precio = et_precio.getText().toString();
+
+        if(!codigo.isEmpty() && !descripcion.isEmpty() && !precio.isEmpty()){
+            ContentValues registro = new ContentValues();
+
+            registro.put("codigo", codigo);
+            registro.put("descripcion", descripcion);
+            registro.put("precio", precio);
+
+            BaseDeDatos.insert("articulos", null, registro);
+
+            BaseDeDatos.close();
+            et_codigo.setText("");
+            et_descripcion.setText("");
+            et_precio.setText("");
+
+            Toast.makeText(this, "Registro exitoso",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Debes llenar todos los campos",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     public void buscar(View view){
-        String nombre = et1.getText().toString();
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+        SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
 
-        SharedPreferences preferencias = getSharedPreferences("agenda",Context.MODE_PRIVATE);
-        String datos = preferencias.getString(nombre, "");
-        if(datos.length() == 0){
-            Toast.makeText(this, "No se encontró ningín registro", Toast.LENGTH_LONG).show();
+        String codigo = et_codigo.getText().toString();
+
+        if(!codigo.isEmpty()){
+            Cursor fila = BaseDeDatos.rawQuery("select descripcion, precio from articulos where codigo = "+ codigo,null);
+
+            if(fila.moveToFirst()){
+                et_descripcion.setText(fila.getString(0));
+                et_precio.setText(fila.getString(1));
+                BaseDeDatos.close();
+            }else{
+                Toast.makeText(this,"No existe el producto", Toast.LENGTH_LONG).show();
+            }
         }else{
-            et2.setText(datos);
+            Toast.makeText(this,"Debe introducir el código del artículo", Toast.LENGTH_LONG).show();
         }
     }
 }
